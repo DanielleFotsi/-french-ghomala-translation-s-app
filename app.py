32b4663c3f0fad8b5e9dcf3f98703b4e553b4fde
Danielle_Laura <laurankonhawe@gmail.com>
2022-07-08 12:26:02 +0200

    web app V1 ok

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify,render_template
+
+import numpy as np
+
+from utils import *
+
+app = Flask(__name__)
+
+
+@app.route('/translate', methods=["POST","GET"])
+def translate():
+    if request.method == "POST":      
+        if request.is_json:
+            request_data = request.json
+            input_text = np.array([request_data['input_text']])
+            source_lang = request_data['source_lang']
+            dest_lang = request_data['dest_lang']
+            
+            translated = make_translation(source_lang, dest_lang, input_text)
+            return jsonify({"result":translated})
+        else:
+            input_text = np.array([request.form['input_text']])
+            source_lang = request.form['source_lang']
+            dest_lang = request.form['dest_lang']
+            
+            translated = make_translation(source_lang, dest_lang, input_text)
+
+            return render_template('index.html', translated=translated, input_text=request.form['input_text'],source_lang=source_lang, dest_lang=dest_lang)
+    elif request.method == "GET":
+        return render_template('index.html')
+    
+    
+@app.route("/", methods=["GET"])
+def home():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(port=8000, debug=True)
---
+++ utils.py
@@ -0,0 +1,50 @@
+import pickle as pk
+
+import numpy as np
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+
+def encode_sequences(tokenizer, length, lines):
+    # integer encode sequences
+	X = tokenizer.texts_to_sequences(lines)
+	# pad sequences with 0 values
+	X = pad_sequences(X, maxlen=length, padding='post')
+	return X
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+	for word, index in tokenizer.word_index.items():
+		if index == integer:
+			return word
+	return None
+ 
+# generate target given source sequence
+def predict_sequence(model, tokenizer, source):
+	prediction = model.predict(source, verbose=0)[0]
+	integers = [np.argmax(vector) for vector in prediction]
+	target = list()
+	for i in integers:
+		word = word_for_id(i, tokenizer)
+		if word is None:
+			break
+		target.append(word)
+	return ' '.join(target)
+
+def make_translation(source_lang, dest_lang, input_text):
+      # Load model
+    model = load_model('models/model_v1.h5')
+    
+    # load model parameters
+    model_parameters = pk.load(open("models/model_parameter_v1.pk","rb"))
+    
+    if source_lang=="en" and dest_lang=="deu" :
+        encode_input =encode_sequences(model_parameters['fren_tokenizer'], model_parameters["gho_length"], input_text)
+        translated = predict_sequence(model,model_parameters["gho_tokenizer"], encode_input)
+        return translated
+    elif source_lang=="deu" and dest_lang=="en": 
+        encode_input = encode_sequences(model_parameters['gho_tokenizer'], model_parameters["gho_length"], input_text)
+        translated = predict_sequence(model,model_parameters["fren_tokenizer"], encode_input)
+        return translated
+    else:
+        return input_text[0]
+    
